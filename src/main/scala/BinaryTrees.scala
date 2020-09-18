7660314262e2d1f819cde617eaf32c92a0b700b1
uhh0701@gmail.com <uhh0701@gmail.com>
2020-09-18 21:54:09 +0900

    add BinaryTrees.scala (29/99)

---
+++ src/main/scala/BinaryTrees.scala
@@ -0,0 +1,30 @@
+sealed abstract class Tree[+T]
+
+case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+  override def toString: String = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+}
+
+case object End extends Tree[Nothing] {
+  override def toString = "."
+}
+
+object Node {
+  def apply[T](value: T): Node[T] = Node(value, End, End)
+}
+
+object Tree {
+  // P55 (**) Construct completely balanced binary trees
+  def cBalanced[T](nodes: Int, value: T): List[Tree[T]] = nodes match {
+    case n if n < 1 => List(End)
+    case n if n % 2 == 1 => {
+      val subtrees = cBalanced(n / 2, value)
+      subtrees.flatMap(l => subtrees.map(r => Node(value, l, r)))
+    }
+    case n if n % 2 == 0 => {
+      val lesserSubtrees = cBalanced((n - 1) / 2, value)
+      val greaterSubtrees = cBalanced((n - 1) / 2 + 1, value)
+      lesserSubtrees.flatMap(l => greaterSubtrees.flatMap(g => List(Node(value, l, g), Node(value, g, l))))
+    }
+  }
+
+}
